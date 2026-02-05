23d91e889c9e15d39937bb025b036faf9ddf1cf7
Soumik Pal <91443670+Soumikcoder@users.noreply.github.com>
2026-02-05 07:07:41 +0000

    added initial files

---
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/message")
+    @CrossOrigin(origins = {"localhost:8080"})
+    public String sendMessage(String message){
+        System.out.println("Message :"+message);
+        return message;
+    }
+}
